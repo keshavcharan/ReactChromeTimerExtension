db28508ccfef365fe192795eff0adc6a9d07ee25
keshavcharan <keshavcharan@gmail.com>
2019-10-14 21:17:03 -0400

    Adding config

---
+++ app/referenceVars/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2pURb1R1vL0v-QqZCCum-Fl2ohV9Lnlc",
+    authDomain: "noname-1553468106326.firebaseapp.com",
+    databaseURL: "https://noname-1553468106326.firebaseio.com",
+    projectId: "noname-1553468106326",
+    storageBucket: "",
+    messagingSenderId: "540244610703",
+    appId: "1:540244610703:web:fd6a759b2a3cc114"
+};
+
+exports.firebaseConfig=firebaseConfig
